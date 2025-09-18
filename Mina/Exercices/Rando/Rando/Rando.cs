ee2d3572e9c9a6aec47baf2b0be2e90e1fd8ac9c
mina-etml <pk60ftv@eduvaud.ch>
2025-09-18 10:08:06 +0200

    add(Rando): exercice Rando)

---
+++ Mina/Exercices/Rando/Rando/Rando.cs
@@ -0,0 +1,73 @@
+using Gpx;
+
+namespace Rando
+{
+    public partial class Rando : Form
+    {
+        List<TrackPoint> trackPoints = new();
+
+        public Rando()
+        {
+            InitializeComponent();
+
+            string gpxFile = @"..\..\..\gpx\Ballade_châtaignère.gpx";
+
+            if (!File.Exists(gpxFile))
+            {
+                MessageBox.Show($"Fichier {gpxFile} n'est pas trouvés");
+            }
+
+            var streamReader = new StreamReader(gpxFile);
+
+
+            using (GpxReader reader = new GpxReader(streamReader.BaseStream))
+            {
+                while (reader.Read())
+                {
+                    switch (reader.ObjectType)
+                    {
+                        case GpxObjectType.Track:
+                        var gpxPoints = reader.Track.ToGpxPoints();
+
+                            //TODO : convrtir les gpxPoints en points avec un SELECT
+                            var converted = gpxPoints.
+                                Select(p => new TrackPoint
+                                {
+                                    Elevation = p.Elevation,
+                                    Latitude = p.Latitude * 10000,
+                                    Longitude = p.Longitude * 10000 
+                                });
+
+                            trackPoints.AddRange(converted.ToList());
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void Rando_Form_Paint(object sender, PaintEventArgs e)
+        {
+            Pen myPen = new Pen(Color.Red);
+            myPen.Width = 2;
+
+            var minLat = trackPoints.Min(tp => tp.Latitude);
+            var maxLat = trackPoints.Max(tp => tp.Latitude);
+            var rangeLat = maxLat - minLat;
+            var ratioLat = rangeLat / Width;
+
+            var minLong = trackPoints.Min(tp => tp.Longitude);
+            var maxLong = trackPoints.Max(tp => tp.Longitude);
+            var rangeLong = maxLong - minLong;
+            var ratioLong = rangeLong / Height;
+
+            //Trackpoint vers points
+            Point[] points = trackPoints.Select(t => new Point()
+            {
+                X = Convert.ToInt32((t.Latitude - minLat) * ratioLat),
+                Y = Convert.ToInt32((t.Longitude - minLong) * ratioLong)
+            }). ToArray();
+
+            this.CreateGraphics().DrawLines(myPen, points);
+        }
+    }
+}
---
+++ Mina/Exercices/Rando/Rando/Trackpoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rando
+{
+    public class TrackPoint
+    {
+        private double _latitude;
+        private double _longitude;
+        private double? _elevation;
+
+        public double Latitude { get => _latitude; set => _latitude = value; }
+        public double Longitude { get => _longitude; set => _longitude = value; }
+        public double? Elevation { get => _elevation; set => _elevation = value; }
+    }
+}
